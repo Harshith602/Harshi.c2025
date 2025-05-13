32ea7870e23494af043c621b258d9a2d3d61a30d
Harshith602 <hrshthlck@gmail.com>
2025-05-13 16:01:33 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Hi Maddy!\n");
+    printf("Enter the values of a and b:");
+    scanf("%d%d", &a, &b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("The swapping result:\n");
+    printf("a is:%d \nb is:%d\n", a, b);
+    return 0;
+}
