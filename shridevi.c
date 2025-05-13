516698b5c0865f656f9949239fd967756974dd6c
Harshith602 <hrshthlck@gmail.com>
2025-05-13 15:42:24 +0530

    Create shridevi.c

---
+++ shridevi.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+char sridevi()
+{
+    printf(" Name=Harshi\n");
+     printf(" DOB=11:11:2006\n");
+      printf(" Mobile no=6361994624\n");
+      printf("  Age=19\n");
+}
+int main()
+{
+    sridevi();
+    return 0;
+}
